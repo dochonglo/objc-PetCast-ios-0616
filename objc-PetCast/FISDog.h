54fb78d344fbe0a866ea4a4d812bdbda702254fb
dochonglo <ismaelbarry777@hotmail.com>
2016-06-16 10:49:00 -0400

    Pet Cast Lab is Completed (All Tests Passed).

---
+++ objc-PetCast/FISDog.h
@@ -0,0 +1,13 @@
+//
+//  FISDog.h
+//  objc-PetCast
+//
+//  Created by Ismael Barry on 6/16/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import "FISPet.h"
+
+@interface FISDog : FISPet
+
+@end
---
+++ objc-PetCast/FISPet.h
@@ -0,0 +1,13 @@
+//
+//  FISPet.h
+//  objc-PetCast
+//
+//  Created by Ismael Barry on 6/16/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FISPet : NSObject
+
+@end
